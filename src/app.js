0ddf3fac29590ae19538a1904a7b7c2657a3967a
jongvnnar <jgh060@gmail.com>
2022-03-04 20:17:09 +0000

    cors

--- src/app.js
+++ src/app.js
@@ -33,8 +33,17 @@ app.use((req, res, next) => {
 
 app.use('/users', userRouter);
 app.use('/events', eventsRouter);
-// app.use('/admin', adminRouter);
-// app.use('/', indexRouter);
+
+// cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 
 /** Middleware sem sér um 404 villur. */
 app.use((req, res) => {
